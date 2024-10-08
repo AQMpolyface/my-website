d12f187691d79d96205c1e265e900ca7a324d05a
AQMpolyface <jonas.morisoli@proton.me>
2024-10-08 11:12:05 +0200

    added css

--- backend.go
+++ backend.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
-
-const savefile string = "/home/polyface/Desktop/go/site2/data.json"
-const jsonDb string = "/home/polyface/Desktop/go/site2/db.json"
 
 func main() {
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/about", aboutHandler)
 
-	http.Handle("/uwu", http.FileServer(http.Dir("./")))
-
-	fmt.Println("Server started at :80")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	fmt.Println("Server started at :8008")
+	log.Fatal(http.ListenAndServe(":8008", nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,28 +37,4 @@ fmt.Fprint(w, string(aboutData))
 
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	if r.Method == http.MethodOptions {
-		return
-	}
-	if r.Method == http.MethodPost {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Unable to read request body", http.StatusBadRequest)
-			return
-		}
-		defer r.Body.Close()
-
-		fmt.Println(string(body))
-
-		response := "uwu"
-		fmt.Fprint(w, response)
-	}
-}
-
-// C
